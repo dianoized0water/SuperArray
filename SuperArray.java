28c164fe0084948c1924ce561b65f2893c16cc0e
diana_yang <dyang20@stuy.edu>
2020-10-31 15:47:51 -0400

    started

---
+++ SuperArray.java
@@ -0,0 +1,17 @@
+public class SuperArray{
+  private String[] data;
+  private int size;
+
+  public SuperArray(){
+    size = 0;
+    String[] superArray = new int[10]
+  }
+
+  public int size(){
+    return size;
+  }
+
+  public boolean add(String element){
+    
+  }
+}
