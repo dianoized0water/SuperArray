9d4e13c65364be5c13914faed1181c7259c6ef9b
diana_yang <dyang20@stuy.edu>
2020-10-31 23:38:43 -0400

    draft one

--- SuperArray.java
+++ SuperArray.java
@@ -4,7 +4,7 @@ public class SuperArray{
 
   public SuperArray(){
     size = 0;
-    String[] superArray = new int[10]
+    String[] data = new String[10];
   }
 
   public int size(){
@@ -12,6 +12,28 @@ public class SuperArray{
   }
 
   public boolean add(String element){
-    
+    if (size+1 == data.length){
+      data.resize();
+    }
+    data[size] = element;
+    size ++;
+    return true;
+  }
+
+  public String get(int index){
+    return data[index];
+  }
+
+  public String set(int index, String element){
+    data[index] = element;
+    return data[index];
+  }
+
+  private void resize(){
+    String[] newArray = new String[size*2];
+    for (int i=0; i<data.length; i++){
+      newArray[i] = data[i];
+    }
+    data = newArray;
   }
 }
